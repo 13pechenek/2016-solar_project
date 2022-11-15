c00f94b6e75b0e982a64c39f9b43221d7d2f21f0
13pechenek <sdzav@yandex.ru>
2022-11-15 12:04:29 +0300

    modified:   solar_main.py

--- solar_main.py
+++ solar_main.py
@@ -34,7 +34,7 @@ def execution():
     """
     global physical_time
     global displayed_time
-    recalculate_space_objects_positions(space_objects, time_step.get()*1000000)
+    recalculate_space_objects_positions(space_objects, time_step.get()*10000)
     for body in space_objects:
         update_object_position(space, body)
     physical_time += time_step.get()
